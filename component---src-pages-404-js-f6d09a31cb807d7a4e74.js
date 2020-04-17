2e043502f94e65fac6a1be2136e28b8b3d723b54
PIN-devel <pin954562@gmail.com>
2020-04-17 16:06:38 +0900

    Updates

---
+++ component---src-pages-404-js-f6d09a31cb807d7a4e74.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),o=n("Bl7J"),r=n("vrFN"),s=n("Wbzz");t.default=function(){return l.a.createElement(o.a,null,l.a.createElement(r.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."),l.a.createElement(s.Link,{className:"btn btn-primary",to:"/"},"Go Home"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-f6d09a31cb807d7a4e74.js.map
